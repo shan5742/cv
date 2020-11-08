a7efb474fcf401ace3999615399650a39e084b13
shan5742 <asamshan456@gmail.com>
2020-11-08 12:15:50 +0000

    adding footer styled

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className="flex justify-center pt-8 pb-16">
-      <p className="text-blue-100 text-xl">
-        Made with ❤️ by{" "}
+      <p className="font-mono text-blue-100 text-xl">
+        made with ❤️ by{" "}
         <Link href="https://asamshan.dev/">
-          <a className="font-bold hover:underline">Asam Shan</a>
+          <a className="font-bold hover:underline">asam shan</a>
         </Link>
       </p>
     </div>
